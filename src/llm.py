5b62a658604014df70bed9eb06027d6f9757cfc7
shayanbaigminecraft-max <shayanbaigminecraft@gmail.com>
2026-08-05 04:26:23 +0500

    llm created

---
+++ src/llm.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from langchain_mistralai import ChatMistralAI
+from src.prompt import prompt_template
+
+load_dotenv()
+
+def generate_response(context, query):
+
+    llm = ChatMistralAI(
+        api_key=os.getenv("mistral"),
+        model="mistral-large-latest",
+        temperature=0.8
+    )
+
+    chain = prompt_template | llm
+
+    answer = chain.invoke({
+        "context": context,
+        "question": query
+    })
+
+    return answer
