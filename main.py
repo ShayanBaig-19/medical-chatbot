92293f999dbbe7d019119597421ea0cb89b3b75f
shayanbaigminecraft-max <shayanbaigminecraft@gmail.com>
2026-08-05 06:03:33 +0500

    fast api aded

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import logging
+from src import logger
 from src.retrival import retrieve_documents
 from src.llm import generate_response
 from src.memory import add_message, get_history
 from src.query_rewrite import rewrite_query
+
+
+logger = logging.getLogger(__name__)
+
+logger.info("Application started")
 
 
 while True:
@@ -14,7 +21,13 @@ while True:
     chat_history = get_history()
 
     new_query = rewrite_query(chat_history, query)
-    context = retrieve_documents(new_query)
+
+    try:
+        context = retrieve_documents(new_query)
+
+    except Exception as e:
+        logger.error(f"Retrieval error: {e}")
+        context = ""
 
     answer = generate_response(
         context,
